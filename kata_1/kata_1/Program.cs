7ba4f9f23129f2e3e1f96bc6263e80d1422cbf05
gugmin <149454489+gugmin@users.noreply.github.com>
2023-11-17 08:51:11 +0900

    kata_1

---
+++ kata_1/kata_1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace kata_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = "";
+            if (num % 2 == 0)
+            {
+                answer = "Even";
+            }
+            else
+                answer = "Odd";
+
+            return answer;
+        }
+    }
+}
