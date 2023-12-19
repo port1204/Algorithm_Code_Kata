c497d6a264e5b9fc910f2e13a79c567e87ebc5a6
gugmin <149454489+gugmin@users.noreply.github.com>
2023-12-19 09:10:11 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.19 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i % 2 == 0)
+            {
+                answer += "수";
+            }
+            else if(i % 2 == 1)
+            {
+                answer += "박";
+            }
+        }
+        return answer;
+    }
+}
