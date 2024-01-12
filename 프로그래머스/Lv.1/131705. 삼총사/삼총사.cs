8a77f1e3a00a388026a7c2fdb0e3938def445838
gugmin <149454489+gugmin@users.noreply.github.com>
2024-01-12 09:26:53 +0900

    [level Lv.1] Title: 삼총사, Time: 0.18 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/Lv.1/131705. 삼총사/삼총사.cs
+++ 프로그래머스/Lv.1/131705. 삼총사/삼총사.cs
@@ -1,13 +1,15 @@
 public class Solution {
     public int solution(int[] number) {
         int answer = 0;
+        int sum = 0;
         for(int i = 0; i < number.Length; i++)
         {
-            for(int j = i + 1; j < number.Length; j++)
+            for(int j = (i + 1); j < number.Length; j++)
             {
-                for(int k = j + 1; k < number.Length; k++)
+                for(int k = (j + 1); k < number.Length; k++)
                 {
-                    if(number[i] + number[j] + number[k] == 0)
+                    sum = number[i] + number[j] + number[k];
+                    if(sum == 0)
                     {
                         answer++;
                     }
