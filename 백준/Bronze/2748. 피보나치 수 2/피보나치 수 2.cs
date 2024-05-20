7316556dda4925ca8dd0870c5240420c01e593b1
gugmin <149454489+gugmin@users.noreply.github.com>
2024-05-20 13:23:27 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 64 ms, Memory: 5592 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cs
@@ -0,0 +1,24 @@
+using System.Text;
+
+namespace CodingTest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            int N = int.Parse(Console.ReadLine());
+            long[] F = new long[N+2];
+            F[0] = 0;
+            F[1] = 1;
+            for (int i = 2; i <= N; ++i)
+            {
+                F[i] = F[i -1] + F[i - 2];
+            }
+            sb.AppendLine($"{F[N]}");
+            Console.Clear();
+            Console.WriteLine(sb.ToString());
+
+        }//main
+    }
+}
