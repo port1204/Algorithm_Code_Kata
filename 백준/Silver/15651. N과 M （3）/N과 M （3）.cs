86cb40967667d6324c835da1f130039438349a90
gugmin <149454489+gugmin@users.noreply.github.com>
2024-07-22 13:35:13 +0900

    [Silver III] Title: N과 M (3), Time: 2256 ms, Memory: 60728 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cs
@@ -0,0 +1,39 @@
+using System.Text;
+
+namespace testcoding
+{
+    internal class Program
+    {
+        static StringBuilder sb = new StringBuilder(); 
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            List<int> sequence = new List<int>();
+
+            GenerateSequences(N, M, 1, sequence);
+
+            Console.WriteLine(sb);
+        }
+
+        static void GenerateSequences(int N, int M, int start, List<int> sequence)
+        {
+            if (sequence.Count == M)
+            {
+               sb.AppendLine(string.Join(" ", sequence));
+                return;
+            }
+
+            for (int i = start; i <= N; i++)
+            {
+                sequence.Add(i);
+
+                GenerateSequences(N, M, 1, sequence);
+
+                sequence.RemoveAt(sequence.Count - 1);
+            }
+        }
+    }
+}
