371d569bb0300e456b44e7f2144f708f0f44a1fa
gugmin <149454489+gugmin@users.noreply.github.com>
2024-07-23 13:35:05 +0900

    [Silver III] Title: N과 M (7), Time: 1676 ms, Memory: 76060 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cs
@@ -0,0 +1,50 @@
+using System.Text;
+
+namespace testcoding
+{
+    internal class Program
+    {
+        static StringBuilder sb = new StringBuilder();
+
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput()))
+            { 
+                string[] input = sr.ReadLine().Split();
+                int N = int.Parse(input[0]);
+                int M = int.Parse(input[1]);
+
+                string[] input2 = sr.ReadLine().Split();
+                int[] nums = new int[N];
+                for (int i = 0; i < N; ++i)
+                {
+                    nums[i] = int.Parse(input2[i]);
+                }
+                Array.Sort(nums);
+
+                GenerateSequences(N, M, nums, new List<int>());
+
+                sw.Write(sb);
+            }
+        }
+
+        static void GenerateSequences(int N, int M, int[] nums, List<int> sequence)
+        {
+            if (sequence.Count == M)
+            {
+               sb.AppendLine(string.Join(" ", sequence));
+                return;
+            }
+
+            for (int i = 0; i < N; ++i)
+            {
+                sequence.Add(nums[i]);
+
+                GenerateSequences(N, M, nums, sequence);
+
+                sequence.RemoveAt(sequence.Count - 1);
+            }
+        }
+    }
+}
