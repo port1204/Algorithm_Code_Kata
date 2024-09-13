cf3ef8154b240bffc0a5e3ca0827c2c31cd17d88
gugmin <149454489+gugmin@users.noreply.github.com>
2024-09-13 13:12:14 +0900

    [Bronze V] Title: A+B - 8, Time: 56 ms, Memory: 6364 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,23 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                int N = int.Parse(sr.ReadLine());
+                for (int i = 0; i < N; ++i)
+                {
+                    string[] input = sr.ReadLine().Split();
+                    int A = int.Parse(input[0]);
+                    int B = int.Parse(input[1]);
+                    sw.WriteLine($"Case #{i + 1}: {A} + {B} = {A + B}");
+                }
+            }
+        }
+
+
+    }
+}
