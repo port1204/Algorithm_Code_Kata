8129aec44fc528a9e08e2d9595b881d95ee489b1
gugmin <149454489+gugmin@users.noreply.github.com>
2024-09-14 15:21:20 +0900

    [Bronze V] Title: 꼬마 정민, Time: 48 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,21 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                string[] input = sr.ReadLine().Split();
+                long A = long.Parse(input[0]);
+                long B = long.Parse(input[1]);
+                long C = long.Parse(input[2]);
+
+                sw.WriteLine($"{A + B + C}");
+            }
+        }
+
+
+    }
+}
