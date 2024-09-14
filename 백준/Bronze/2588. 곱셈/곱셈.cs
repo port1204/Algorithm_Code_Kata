37dbaf9f207d202376a2b1cf75fa51d8d8ffae01
gugmin <149454489+gugmin@users.noreply.github.com>
2024-09-14 15:17:25 +0900

    [Bronze III] Title: 곱셈, Time: 48 ms, Memory: 5532 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cs
@@ -0,0 +1,22 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                int N = int.Parse(sr.ReadLine());
+                int M = int.Parse(sr.ReadLine());
+
+                sw.WriteLine($"{N * (M % 10)}");
+                sw.WriteLine($"{N * ((M / 10) % 10)}");
+                sw.WriteLine($"{N * (M / 100)}");
+                sw.WriteLine($"{N * M}");
+            }
+        }
+
+
+    }
+}
