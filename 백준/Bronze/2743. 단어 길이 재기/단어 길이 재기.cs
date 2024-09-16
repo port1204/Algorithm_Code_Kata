36e2846f293d5b75620e67f1ffc9145b0ad77551
gugmin <149454489+gugmin@users.noreply.github.com>
2024-09-16 20:35:05 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cs
@@ -0,0 +1,17 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                string input = sr.ReadLine();
+                sw.WriteLine(input.Length);
+            }
+        }
+
+
+    }
+}
