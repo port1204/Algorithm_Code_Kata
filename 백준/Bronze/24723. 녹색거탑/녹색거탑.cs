695690090737b51b18bc0b5eec42ce3881cde0a1
gugmin <149454489+gugmin@users.noreply.github.com>
2024-09-26 13:56:05 +0900

    [Bronze IV] Title: 녹색거탑, Time: 48 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cs
@@ -0,0 +1,16 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                int N = int.Parse(sr.ReadLine());
+                sw.WriteLine(Math.Pow(2, N));
+            }
+        }
+
+    }
+}
