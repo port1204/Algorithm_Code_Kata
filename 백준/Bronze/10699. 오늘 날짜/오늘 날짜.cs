6fdaf24839b56b1ce3544ed5838064d148bd1fb4
gugmin <149454489+gugmin@users.noreply.github.com>
2024-10-23 15:29:01 +0900

    [Bronze V] Title: 오늘 날짜, Time: 48 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cs
@@ -0,0 +1,17 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                DateTime today = DateTime.Now;
+                sw.WriteLine(today.ToString("yyyy-MM-dd"));
+            }
+        }
+
+
+    }
+}
