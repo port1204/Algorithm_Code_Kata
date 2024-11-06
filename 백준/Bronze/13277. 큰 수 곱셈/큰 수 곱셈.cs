fc1eab1472b50f15338bbb938a88a43504540af0
gugmin <149454489+gugmin@users.noreply.github.com>
2024-11-06 12:33:51 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5656 ms, Memory: 30284 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.cs
@@ -0,0 +1,21 @@
+using System.Numerics;
+
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                string[] input = sr.ReadLine().Split();
+                BigInteger A = BigInteger.Parse(input[0]);
+                BigInteger B = BigInteger.Parse(input[1]);
+                sw.WriteLine(A * B);
+            } // using
+        } // Main
+
+
+    }
+}
