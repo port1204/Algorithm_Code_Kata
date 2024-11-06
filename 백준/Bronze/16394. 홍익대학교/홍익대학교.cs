6d5ecaed1b121bff21bab16d14212a646303606a
gugmin <149454489+gugmin@users.noreply.github.com>
2024-11-06 15:41:13 +0900

    [Bronze V] Title: 홍익대학교, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cs
@@ -0,0 +1,18 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                int N = int.Parse(sr.ReadLine());
+                sw.WriteLine(N - 1946);
+
+            } // using
+        } // Main
+
+
+    }
+}
