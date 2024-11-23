9eb37cf4ba770e603d9d7e9436414e6e43c75609
gugmin <149454489+gugmin@users.noreply.github.com>
2024-11-23 12:31:25 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cs
@@ -0,0 +1,17 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                int[] input = Array.ConvertAll(sr.ReadLine().Split(), int.Parse);
+                int SA = Math.Min(input[0] / 2, input[1] / 2);
+                sw.WriteLine(SA);   
+
+            } // using
+        } // Main
+    } // class
+} // namespace
