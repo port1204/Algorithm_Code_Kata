ddf251417e92c2635c552eb8e87fe2c8868ca16b
gugmin <149454489+gugmin@users.noreply.github.com>
2025-03-13 14:20:22 +0900

    [Bronze V] Title: Adding Trouble, Time: 48 ms, Memory: 5520 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cs
@@ -0,0 +1,27 @@
+namespace testcoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(Console.OpenStandardInput()))
+            using (StreamWriter sw = new StreamWriter(Console.OpenStandardOutput(), bufferSize: 1048576))
+            {
+                string[] input = sr.ReadLine().Split();
+                int x = int.Parse(input[0]);
+                int y = int.Parse(input[1]);
+                int z = int.Parse(input[2]);
+                if (x + y == z)
+                {
+                    sw.WriteLine("correct!");
+                }
+                else
+                {
+                    sw.WriteLine("wrong!");
+                }
+
+
+            } // using
+        } // Main
+    } // class
+} // namespace
